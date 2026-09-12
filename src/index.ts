0546e6733fe95536eff6fc54d2a089af27ef22d6
il4mb <durianbohong@gmail.com>
2026-09-12 16:18:49 +0700

    Remove TupleList implementation and clean up exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ export type * from "./type";
 export * from "./registry";
 export * from "./tokenizer";
 export * from "./tools";
-export * from "./tupleList";
-
 
 const tokenizer = new Tokenizer(new Registry());
 export const tokenizeImpl = (content: string) => tokenizer.tokenize(content);
